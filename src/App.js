f69b40c62180f608a6dd4c30a16e7170402c664f
kritika <kritikasri27@gmail.com>
2021-03-07 18:44:21 +0530

    Added main file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import DarkModeToggle from "react-dark-mode-toggle";
+import Categories from "./components/Categories";
+import data from "./data";
+import Menu from "./components/Menu";
+import "./index.css";
 
-function App() {
+export default function App() {
+  const [menu, setMenu] = useState(data);
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const categories = ["all", ...new Set(data.map((item) => item.category))];
+
+  
+  const filterMenu = (category) => {
+    if (category === "all") {
+      setMenu(data);
+    } else {
+      const newItems = data.filter((item) => item.category === category);
+      setMenu(newItems);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={isDarkMode ? "menu section dark" : "menu section light"}
+    >
+      <div className={isDarkMode ? "title dark" : "title light"}>
+        <h2>Menu Card</h2>
+
+        <DarkModeToggle
+          className="btn"
+          onChange={() => setIsDarkMode(!isDarkMode)}
+          checked={isDarkMode}
+          size={75}
+        />
+      </div>
+      <Categories category={categories} filterMenu={filterMenu} />
+      <Menu menu={menu} isDarkMode={isDarkMode} />
     </div>
   );
 }
-
-export default App;
