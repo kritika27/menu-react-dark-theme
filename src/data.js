dc9cee9d7f424639e5f3fe7e8c3d520a471c38b0
kritika <kritikasri27@gmail.com>
2021-03-07 18:40:11 +0530

    Added the data containing restaurant menu details

---
+++ src/data.js
@@ -0,0 +1,84 @@
+const menu = [
+  {
+    id: 1,
+    title: "buttermilk pancakes",
+    category: "breakfast",
+    price: 10,
+    img: "./images/amirali-mirhashemian-sc5sTPMrVfk-unsplash.jpg",
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore id, ratione necessitatibus voluptatem corporis similique aliquam. Debitis explicabo officia perferendis?",
+  },
+  {
+    id: 2,
+    title: "diner double",
+    category: "lunch",
+    price: 10,
+    img: "./images/dan-counsell-t4NNQyv09Sw-unsplash.jpg",
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore id, ratione necessitatibus voluptatem corporis similique aliquam. Debitis explicabo officia perferendis?",
+  },
+  {
+    id: 3,
+    title: "godzilla milkshake",
+    category: "snacks",
+    price: 9,
+    img: "./images/ibrahim-boran-Q9KUfbgzGnA-unsplash.jpg",
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore id, ratione necessitatibus voluptatem corporis similique aliquam. Debitis explicabo officia perferendis?",
+  },
+  {
+    id: 4,
+    title: "country delight",
+    category: "breakfast",
+    price: 21,
+    img: "./images/jay-wennington-N_Y88TWmGwA-unsplash.jpg",
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore id, ratione necessitatibus voluptatem corporis similique aliquam. Debitis explicabo officia perferendis?",
+  },
+  {
+    id: 5,
+    title: "egg attack",
+    category: "lunch",
+    price: 25,
+    img: "./images/kimia-zarifi-pcST1a65ekI-unsplash.jpg",
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore id, ratione necessitatibus voluptatem corporis similique aliquam. Debitis explicabo officia perferendis?",
+  },
+  {
+    id: 6,
+    title: "oreo dream",
+    category: "snacks",
+    price: 15,
+    img: "./images/louis-hansel-shotsoflouis-qNBGVyOCY8Q-unsplash.jpg",
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore id, ratione necessitatibus voluptatem corporis similique aliquam. Debitis explicabo officia perferendis?",
+  },
+  {
+    id: 7,
+    title: "bacon overflow",
+    category: "breakfast",
+    price: 9,
+    img: "./images/sam-moqadam-wg551YMfeOc-unsplash.jpg",
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore id, ratione necessitatibus voluptatem corporis similique aliquam. Debitis explicabo officia perferendis?",
+  },
+  {
+    id: 8,
+    title: "american classic",
+    category: "lunch",
+    price: 13,
+    img: "./images/item-8.jpeg",
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore id, ratione necessitatibus voluptatem corporis similique aliquam. Debitis explicabo officia perferendis?",
+  },
+  {
+    id: 9,
+    title: "surprise menu",
+    category: "snacks",
+    price: 11,
+    img: "./images/item-8.jpeg",
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore id, ratione necessitatibus voluptatem corporis similique aliquam. Debitis explicabo officia perferendis?",
+  },
+];
+export default menu;
